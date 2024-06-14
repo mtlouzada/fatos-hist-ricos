a92274675705b04781e891fe0d8cfe49bdf34395
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-14 10:43:57 -0300

    imputs em exports

---
+++ data/data.js
@@ -0,0 +1,6 @@
+let turingQuestions = [
+    {id: 1, ask: "Em qual ano nasceu Alan Mathison Turing, em Londres, Inglaterra ?",  answer: 1912},
+    {id: 2, ask:""}
+];
+
+export default turingQuestions;
