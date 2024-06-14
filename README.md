d666b0cb0a4a398f224801174a612dac4d401d53
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-14 17:14:20 -0300

    15 questions about turing

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Status: Developing process ⚠️
 [![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity) [![Website fakesite.invalid](https://img.shields.io/website-up-down-green-red/http/fakesite.invalid.svg)](http://fakesite.invalid/) [![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)](https://GitHub.com/Naereen/ama)
 
-_Read this in other languages:_ 🇧🇷 [_Português_](./translations/README-ptBR.md) 🇧🇷
+_Read this in other languages:_ 🇧🇷 [_Português_](./translations/README-ptBR.md) 🟢🟡
--- data/data.js
+++ data/data.js
@@ -1,6 +1,19 @@
 let turingQuestions = [
     {id: 1, ask: "Em qual ano nasceu Alan Mathison Turing, em Londres, Inglaterra ?",  answer: 1912},
-    {id: 2, ask:""}
+    {id: 2, ask:"Aos 13 anos Turing, teve a oportunidade de estudar na Sherborne School, uma prestigiada escola da capital inglesa. Em que ano isso aconteceu?", answer: 1926},
+    {id: 3, ask: "No seu primeiro dia de aula na Sherborne School, uma greve impediu que os trens circulassem, e Turing pedalou quase 100 quilômetros de onde morava para não perder a aula. Em que ano isso aconteceu?", answer: 1925},
+    {id: 4, ask: "Até que ano Turing estudou na Sherbone School?", answer: 1931},
+    {id: 5, ask: "Em que ano, Turing foi para a universidade Kings College, em Cambridge, Inglaterra?", answer: 1931},
+    {id: 6, ask: "O problema de incompletude da matemática é: A existência de afirmações verdadeiras sobre números que não puderam ser provadas formalmente por meio das regras estabelecidas de dedução. Em que ano Turing se deparou com isso pela primeira vez?", answer: 1931},
+    {id: 7, ask: "5 anos depois de se deparar com o problema da incompletude matemática, a ideia da máquina de Turing ficou pronta. Em que ano isso aconteceu?", ask: 1936},
+    {id: 8, ask: "Com o início da Segunda Guerra Mundial, Turing retorna à Cambridge, onde trabalha secretamente para o Departamento Criptanalítico Bretão. Em que ano isso ocorreu?", answer: 1938},
+    {id: 9, ask: "Em que ano Von Neumann, um grande matemático aplicou os princípios de Turing, fazendo com que o mundo tivesse sua primeira apresentação prática de um computador?", answer: 1948},
+    {id: 10, ask: "Em qual ano Turing continuou demonstrando notável capacidade nos estudos que amava, resolvendo problemas avançados, sem ter estudado sequer cálculo elementar?", answer: 1927},
+    {id: 11, ask: "Aos 16 anos, Turing entrou em contato com o trabalho de Albert Einstein; não apenas ele o compreendeu, mas conseguiu deduzir o questionamento de Einstein das leis de Newton. Em que ano isso ocorreu?", answer: 1928},
+    {id: 12, ask: "Em que ano, Turingo perdeu seu grande amigo Christopher Collan Morcom, que morreu de de complicações da tuberculose bovina, contraída após beber o leite de vaca infectado alguns anos antes.", answer: 1930},
+    {id: 13, ask: "3 anos após a morte de seu grande amigo, Turing mandou uma carta para mãe de Morcom, mostrando que seu relacionanmento com ela contuniou muito depois da morte dele. Em que ano essa carta foi enviada?", answer: 1933},
+    {id: 14, ask: "Em cambridge, Turing foi agraciado com honras de primeira classe em matemática. Em que ano isso ocorreu?", answer: 1934},
+    {id: 15, ask: "Com apenas 22 anos, foi eleito fellow do King's College com base em uma dissertação na qual provou o teorema central do limite. Fato desconhecido pelo comitê anos depois. Em que ano ele foi eleito fellow?", answer: 1935}
 ];
 
 export default turingQuestions;
